70417e03d140ae5c33f58bfc529ee159d06e88e5
wandeson <wandesonjsantos7@gmail.com>
2023-08-07 23:35:27 -0300

    Service layer, component registration and Order, Instant, ISO

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,11 +1,16 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,8 +23,12 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
-    public User(){
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
+    public User(){
     }
 
     public User(Long id, String name, String email, String phone, String password) {
@@ -70,6 +79,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+		return orders;
+	}
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -94,8 +107,4 @@ public class User implements Serializable {
             return false;
         return true;
     }
-
-
-
-    
 }
